9d7cfbf21c74a09ebecf50dd9d6b463ad35c6e97
cross-xross <to.kawashima0207@gmail.com>
2016-02-28 21:37:19 +0900

    Add show edit screen event.

--- www/js/index.js
+++ www/js/index.js
@@ -3,5 +3,8 @@ var ViewModel = {
 		{ startTime: "09:00", location: "渋谷", arrivalTime: "10:05" },
 		{ startTime: "10:21", location: "横浜", arrivalTime: "12:30" },
 		{ startTime: "13:05", location: "平塚", arrivalTime: "15:00" }
-    ])
+    ]),
+    inputStartTime: ko.observable(""),
+    inputLocation: ko.observable("Sample"),
+    inputArrivalTime: ko.observable("")
 };
